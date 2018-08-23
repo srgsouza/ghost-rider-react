c70a1d1249255935622fde7c280e6fccab73698a
Alex <fuckyobitch@Alexs-MacBook-Pro-2.local>
2018-08-23 10:35:47 -0600

    added edit comment modal

--- ghostRider-React/ghost-rider-react/src/CarsContainer/Comments.js
+++ ghostRider-React/ghost-rider-react/src/CarsContainer/Comments.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Container, Row, Col, Button } from 'reactstrap';
+import { Container, Row, Col, Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import classes from './Comments.css';
+import EditComment from './CRUDComments/EditComment';
 
 const Comments = (props) => {
 
@@ -15,7 +16,22 @@ const Comments = (props) => {
           <div key={comment.id} className='specificComment'>
             <span>{comment.comment}</span><br />
             <Button color='danger' onClick={props.deleteComment.bind(null, comment.id)}>Delete</Button>
-            <Button color='danger' onClick={props.showCommentModal.bind(null, comment.id)}>Edit</Button>
+
+
+
+            <div className="addCarBTN">
+              <Button color="danger" onClick={props.showCommentModal.bind(null, comment.id)}>Edit Car</Button>
+              <Modal isOpen={props.modal2} toggle1={props.toggle1}>
+                <ModalHeader className="modal-header" toggle1={props.toggle2}>Edit Your Car Below:</ModalHeader>
+                <ModalBody>
+
+                <EditComment closeAndEditComment={props.closeAndEditComment}
+                             handleCommentFormChange={props.handleCommentFormChange}
+                             commentToEdit={props.state.commentToEdit}
+                />
+
+
+
           </div>
         </Col>
 
