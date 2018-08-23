f67c76fe97fb9249bbfa2e1278101966f3fab478
Sergio Souza <emailsouza@gmail.com>
2018-08-22 18:18:29 -0600

    bug fixes, added email address to register, remove cam file upload

--- ghostRider-React/ghost-rider-react/src/CarsContainer/Comments.js
+++ ghostRider-React/ghost-rider-react/src/CarsContainer/Comments.js
@@ -7,7 +7,7 @@ const Comments = (props) => {
   let apiCarID = 'http://127.0.0.1:8000/users/api/cars/' + props.carID;
 
   const commentList = props.comments.map((comment, i) => {
-    console.log(comment, ' comment id, this is why we needed the checkbox, will discuss')
+    // console.log(comment, ' comment id, this is why we needed the checkbox, will discuss')
     if (apiCarID === comment.car) {
       return (
 
--- ghostRider-React/ghost-rider-react/src/Login/Logout.js
+++ ghostRider-React/ghost-rider-react/src/Login/Logout.js
@@ -1,46 +1,43 @@
 /* eslint react/no-multi-comp: 0, react/prop-types: 0 */
 import React from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter, NavItem, NavLink, Form, FormGroup, Label, Input } from 'reactstrap';
-class Register extends React.Component {
+class Login extends React.Component {
   constructor(props) {
-    super(props);
+    super();
     this.state = {
-      modal: false
+      modal: false,
     };
     this.toggle = this.toggle.bind(this);
   }
+
   toggle() {
     this.setState({
       modal: !this.state.modal
     });
   }
+
   render() {
     return (
       <div>
         <NavItem>
-          <Button color="link" onClick={this.toggle}>Register</Button>
+          <Button className="nav-links" onClick={this.toggle}>Logout</Button>
         </NavItem>
         <Modal isOpen={this.state.modal} modalTransition={{ timeout: 400 }} backdropTransition={{ timeout: 700 }}
           toggle={this.toggle} className={this.props.className}>
-          <ModalHeader toggle={this.toggle}>Register</ModalHeader>
+          <ModalHeader toggle={this.toggle}>Logout</ModalHeader>
           <ModalBody>
-            <Form onSubmit={this.props.handleRegistration} inline>
-              <FormGroup>
-                <Label for="exampleUser" hidden>Username</Label>
-                <Input type="text" name="username" id="exampleUser" onChange={this.props.handleChange} placeholder="input a username" />
-              </FormGroup>
-              {' '}
+            <Form onSubmit={this.props.handleSubmit} inline>
               <FormGroup>
                 <Label for="exampleEmail" hidden>Email</Label>
-                <Input type="email" name="email" id="exampleEmail" onChange={this.props.handleChange} placeholder="input an email" />
+                <Input type="text" name="username" id="exampleEmail" onChange={this.props.handleChange} placeholder="Email (username)" />
               </FormGroup>
               {' '}
               <FormGroup>
                 <Label for="examplePassword" hidden>Password</Label>
-                <Input type="password" name="password" id="examplePassword" onChange={this.props.handleChange} placeholder="input a password" />
+                <Input type="password" name="password" id="examplePassword" onChange={this.props.handleChange} placeholder="Password" />
               </FormGroup>
               {' '}
-              <Button type="Submit" onClick={this.toggle}>Register</Button>
+              <Button type="submit" onClick={this.toggle}>Login</Button>
             </Form>
           </ModalBody>
         </Modal>
@@ -48,4 +45,4 @@ class Register extends React.Component {
     );
   }
 }
-export default Register;
+export default Login;
